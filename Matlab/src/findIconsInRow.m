8c61fbedea29aac9f27c899e5345401246092a85
Appcell <zhuyiq@student.ethz.ch>
2017-12-18 20:58:25 +0100

    optimize killfeed analysis

--- Matlab/src/findIconsInRow.m
+++ Matlab/src/findIconsInRow.m
@@ -1,63 +1,44 @@
-function charas = findIconsInRow(I, charaNamesArr, iconsArr)
-totalIconsNum = size(iconsArr, 2);
-coeffArr = zeros(totalIconsNum, 1);
-posArr = zeros(totalIconsNum, 4);
-w = size(I, 2);
-h = size(I, 1);
-for i = 1:totalIconsNum
-    icon = iconsArr{i};
-    icon = imresize(icon,17/size(icon, 1));
-%     icon = rgb2gray(imresize(icon,17/size(icon, 1)));
-    IRed = I(:,:,1);
-    IGreen = I(:,:,2);
-    IBlue = I(:,:,3);
-    iconRed = icon(:, :, 1);
-    iconGreen = icon(:, :, 2);
-    iconBlue = icon(:, :, 3);
-%     I = normalizeGrayScaleImg(I);
-%     icon = normalizeGrayScaleImg(icon);
-    c1 = normxcorr2(normalizeGrayScaleImg(iconRed),normalizeGrayScaleImg(IRed));
-    c2 = normxcorr2(normalizeGrayScaleImg(iconGreen),normalizeGrayScaleImg(IGreen));
-    c3 = normxcorr2(normalizeGrayScaleImg(iconBlue),normalizeGrayScaleImg(IBlue));
-    c1Max = max(c1(:));
-    c2Max = max(c2(:));
-    c3Max = max(c3(:));
-    meanC = (c1Max + c2Max + c3Max) / 3;
-%     imshow(iconRed);figure;imshow(IRed);
-    [ypeak, xpeak] = find(c1==max(c1(:)));
-    
-    yoffSet = ypeak - size(icon,1);
-    xoffSet = xpeak - size(icon,2);
-    if isInImage([xoffSet+1, yoffSet+1, size(icon,2) , size(icon,1)], w, h)
-        posArr(i, :) = [xoffSet+1, yoffSet+1, size(icon,2) , size(icon,1)];
-        coeffArr(i) = meanC;
-    else
-        posArr(i, :) = [xoffSet+1, yoffSet+1, size(icon,2) , size(icon,1)];
-        coeffArr(i) = 0;
+function charas = findIconsInRow(rowNum, Itemp, charaNamesArr, iconsArr)
+
+Iright = imcrop(Itemp, [1145, 110 + (rowNum-1)*35 - 1, 100, 37]);
+charaRight = matchIcon(Iright, charaNamesArr, iconsArr);
+if charaRight.name == "empty"
+    charaLeft = struct('name', "empty", 'pos', [], 'coeff', 0, 'team', 0);
+    charas = {charaLeft, charaRight};
+else
+    Ileft = imcrop(Itemp, [963, 110 + (rowNum-1)*35 - 1, 205, 37]);
+    charaLeft = matchIcon(Ileft, charaNamesArr, iconsArr);
+    color1 = Itemp(1, 1, :);
+    color2 = Itemp(1, 1280, :);
+    if charaLeft.name ~= "empty"
+        pos = charaLeft.pos(1:2);
+        pos = pos - [5 0];
+        color = Ileft(pos(2), pos(1), :);
+        colorTemp1 = abs(double(color1) - double(color));
+        colorTemp2 = abs(double(color2) - double(color));
+        diff1 = max(colorTemp1);
+        diff2 = max(colorTemp2);
+        if diff1 <= diff2
+            charaLeft.team = 1;
+        else
+            charaLeft.team = 2;
+        end
     end
-    
-
-%     imrect(gca, [xoffSet+1, yoffSet+1, size(icon,2) , size(icon,1)]);
-%     figure
-%     imshow(I);
-%     imrect(gca, [xoffSet+1, yoffSet+1, size(icon,2) , size(icon,1)]);
-end
-
-[sortedCoeffs, sortedInds] = sort(coeffArr(:),'descend');
-top2 = sortedInds(1:2);
-charas = [];
-% Choose only top 2 icons
-for i = 1:2
-    charas(i).name = charaNamesArr(top2(i));
-    charas(i).pos = posArr(top2(i), :);
-    charas(i).coeff = coeffArr(top2(i));
-%     figure
-%     imshow(I);
-%     imrect(gca, charas(i).pos);
-end
-
-if charas(1).pos(1) > charas(2).pos(1)
-    charas([1 2]) = charas([2 1]);
+    if charaRight.name ~= "empty"
+        pos = charaRight.pos(1:2);
+        pos = pos + [charaRight.pos(3), 0] + [5 0];
+        color = Iright(pos(2), pos(1), :);
+        colorTemp1 = abs(double(color1) - double(color));
+        colorTemp2 = abs(double(color2) - double(color));
+        diff1 = max(colorTemp1);
+        diff2 = max(colorTemp2);
+        if diff1 <= diff2
+            charaRight.team = 1;
+        else
+            charaRight.team = 2;
+        end  
+    end
+    charas = {charaLeft, charaRight};
 end
 
 end
@@ -68,10 +49,67 @@ function res = isInImage(pos, w, h)
     y = pos(2);
     iw = pos(3);
     ih = pos(4);
-    if x > 0 && x< w ...
-            && y > 0 && y< h ...
-            && x+iw > 0 && x+iw< w ...
-            && y+ih > 0 && y+ ih < h
+    if x >= 0 && x <= w ...
+            && y >= 0 && y <= h ...
+            && x + iw >= 0 && x + iw <= w ...
+            && y + ih >= 0 && y + ih <= h
         res = 1;
     end
+end
+
+function charas = matchIcon(I, charaNamesArr, iconsArr)
+w = size(I, 2);
+h = size(I, 1);
+
+totalIconsNum = size(iconsArr, 2);
+coeffArr = zeros(totalIconsNum, 1);
+posArr = zeros(totalIconsNum, 4);
+for i = 1:totalIconsNum
+    icon = iconsArr{i};
+    icon = imresize(icon,21/size(icon, 1));
+%     IRed = I(:,:,1);
+    IGreen = I(:,:,2);
+%     IBlue = I(:,:,3);
+%     iconRed = icon(:, :, 1);
+    iconGreen = icon(:, :, 2);
+%     iconBlue = icon(:, :, 3);
+%     c1 = normxcorr2(normalizeGrayScaleImg(iconRed),normalizeGrayScaleImg(IRed));
+    c2 = normxcorr2(normalizeGrayScaleImg(iconGreen),normalizeGrayScaleImg(IGreen));
+%     c3 = normxcorr2(normalizeGrayScaleImg(iconBlue),normalizeGrayScaleImg(IBlue));
+%     c1Max = max(c1(:));
+    c2Max = max(c2(:));
+%     c3Max = max(c3(:));
+%     meanC = (c1Max + c2Max + c3Max) / 3;
+    meanC = c2Max;
+    [ypeak, xpeak] = find(c2==max(c2(:)));
+    
+    yoffSet = ypeak - size(icon,1);
+    xoffSet = xpeak - size(icon,2);
+    if isInImage([xoffSet+1, yoffSet+1, size(icon,2) , size(icon,1)], w, h)
+        posArr(i, :) = [xoffSet+1, yoffSet+1, size(icon,2) , size(icon,1)];
+        coeffArr(i) = meanC;
+    else
+        posArr(i, :) = [xoffSet+1, yoffSet+1, size(icon,2) , size(icon,1)];
+        coeffArr(i) = 0;
+    end
+end
+
+[sortedCoeffs, sortedInds] = sort(coeffArr(:),'descend');
+top1 = sortedInds(1:1);
+charas = [];
+% Choose only top 1 icon
+for i = 1:1
+    if coeffArr(top1(i)) > 0.65
+        charas(i).name = charaNamesArr(top1(i));
+        charas(i).pos = posArr(top1(i), :);
+        charas(i).coeff = coeffArr(top1(i));
+        charas(i).team = 0;
+    else
+        charas(i).name = "empty";
+        charas(i).pos = posArr(top1(i), :);
+        charas(i).coeff = coeffArr(top1(i));
+        charas(i).team = 0;
+    end
+end
+
 end
--- Matlab/src/getKillEvents.m
+++ Matlab/src/getKillEvents.m
@@ -1,24 +1,37 @@
-function charas = getKillEvents(v)
-Itemp = readFrame(v);
-
-%% Icons read-in
-charaNamesArr = ["ana", "bastion", "doomfist", "dva", "genji", "hanzo", "junkrat", "lucio", "mccree", "mei", "mercy", "moira", "orisa", "pharah", "reaper", "reinhardt", "riptire", "roadhog", "soldier76", "sombra", "symmetra", "torbjon", "tracer", "widowmaker", "winston", "zarya", "zenyatta", "meka", "shield", "supercharger", "teleporter", "turret"];
-iconsArr = {};
-for i=1:size(charaNamesArr, 2)
-    iconsArr{i} = imread(convertStringsToChars("./../../images/icons/" + charaNamesArr(i) + ".png"));
-end
+function charas = getKillEvents(Itemp, eventList, charaNamesArr, iconsArr)
 
 %% Comparison by row, each row is 30px, 4 rows at most
-charas = struct('name',{},'pos',{}, 'coeff',{});
-for i = 1:4
-    Irow = imcrop(Itemp, [800, 202 + (i-1)*26 - 1, 355, 28]);
-    charasRow = findIconsInRow(Irow, charaNamesArr, iconsArr);
-    charas(end+1, 1) = charasRow(1);
-    charas(end, 2) = charasRow(2);
+charas = {};
+if size(eventList, 1) > 1
+    for i = 1:6
+        charasRow = findIconsInRow(i, Itemp, charaNamesArr, iconsArr);
+        lastChara1 = eventList{end, 1};
+        lastChara2 = eventList{end, 2};
+        if charasRow{1}.name == "empty" && charasRow{2}.name == "empty" && i>1
+            break;
+        end
+        if charasRow{1}.name == lastChara1.name && charasRow{2}.name == lastChara2.name...
+                && charasRow{1}.team == lastChara1.team && charasRow{2}.team == lastChara2.team...
+            break;
+        end
+        charas{end+1, 1} = charasRow{1};
+        charas{end, 2} = charasRow{2};
+
+    end
+else
+    for i = 1:6
+        charasRow = findIconsInRow(i, Itemp, charaNamesArr, iconsArr);
+        charas{end+1, 1} = charasRow{1};
+        charas{end, 2} = charasRow{2};
+        if charasRow{1}.name == "empty" && charasRow{2}.name == "empty" && i>1
+            break;
+        end
+    end
 end
 
+
 %% Output
-% imshow(imcrop(Itemp, [800, 202, 355, 104]));
+% imshow(Itemp);
 % for i = 1:size(charas, 1)
 %     chara1 = charas(i, 1);
 %     chara2 = charas(i, 2);
