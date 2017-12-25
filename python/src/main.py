f3c907b30107db93778f3ae29511d191cae143b4
Xiaochen Li <leave.body@hotmail.com>
2017-12-25 01:36:35 -0500

    Python: Can analyse full video now. Tested on "SDvsNYXL_Preseason.mp4" and mached all killfeeds without any false positive and false negative.

--- python/src/main.py
+++ python/src/main.py
@@ -25,13 +25,12 @@ def analyze_video(video_loader):
         analyzer = FrameAnalyzer(frame, index)
         new_killfeeds = analyzer.get_killfeed(killfeed_list[-1] if len(killfeed_list) > 0 else None)
         killfeed_list.extend(new_killfeeds)
-        index += step
-        frame = video_loader.get_frame(index)
         for k in new_killfeeds:
             print index/video_loader.fps, k
+        index += step
+        frame = video_loader.get_frame(index)
     # for k in killfeed_list:
     #     print k
-
 
 
 class FrameAnalyzer:
@@ -55,17 +54,17 @@ class FrameAnalyzer:
         result = []
         for i in range(5):
             killfeed_in_row = self._get_killfeed_in_row(i)
-            if killfeed_in_row is not None:
-                if killfeed_in_row == killfeed_last:
-                    break
-                result.append(killfeed_in_row)
+            if killfeed_in_row is None or killfeed_in_row == killfeed_last:
+                break
+            result.append(killfeed_in_row)
+        result.reverse()  # Reverse the list to make it from bottom to top.
         return result
 
     def _get_killfeed_in_row(self, row_number):
         # Crop the image and get the desired row.
         # TODO save these parameters as static value
         killfeed_image = image.crop_by_limit(self.frame, 116 + row_number*35, 23, 963, 1270-963)
-        cv2.imshow("row"+str(row_number), killfeed_image)
+        # cv2.imshow("row"+str(row_number), killfeed_image)
         name = "killfeed_image"+str(row_number)
         icons_weights = self._get_icons_weights(killfeed_image, name)
         # TODO Only needs two best matches in the end. Keep three while debugging.
@@ -82,27 +81,35 @@ class FrameAnalyzer:
                 best_matches.append(item)
 
         # Edge detection around the found icons.
-        edge_image = cv2.Canny(killfeed_image,100,200)  # Generate the edges in this killfeed image.
+        edge_image = cv2.Canny(killfeed_image, 100, 200)  # Generate the edges in this killfeed image.
+        # cv2.imshow(str(row_number), edge_image)
         edge_span = np.zeros(edge_image.shape)
         for i in range(edge_image.shape[0]):
-            for j in range(3, edge_image.shape[1] - 1):
-                edge_span[i, j] = sum(edge_image[i, j-3: j+1])  # Get the "spanned" edge image.
+            for j in range(1, edge_image.shape[1]):
+                edge_span[i, j] = sum(edge_image[i, j-1: j+1])  # Get the "spanned" edge image.
         edge_sum = edge_span.sum(0)  # Sum the result on y axis.
         edge_sum = edge_sum/(255*overwatch.ICON_KILLFEED_720P_HEIGHT)  # Normalize the result.
 
         matched_icons = []
         for item in best_matches:
-            if edge_sum[item[2]] >= (overwatch.ICON_KILLFEED_720P_HEIGHT - 2.0)/overwatch.ICON_KILLFEED_720P_HEIGHT:
+            if item[1] < 0.6:  # todo save 0.6 as a constant
+                continue
+            edge_scores = edge_sum[item[2]-2: item[2]+2]
+            threshold = (overwatch.ICON_KILLFEED_720P_HEIGHT - 1.0)/overwatch.ICON_KILLFEED_720P_HEIGHT
+            if max(edge_scores if len(edge_scores) > 0 else [0]) >= threshold:
                 # The metric is that there should be a vertical edge
                 # between left 3 pixels to the icon position
                 # and right 1 pixel to the icon position
-                # with no more than 2 pixels in the vertical line missing.
+                # with no more than 1 pixels in the vertical line missing.
+                # A vertical line should have a width of no more than 2.
                 matched_icons.append(item)
 
-        # print "row",row_number, best_matches, [edge_sum[best_matches[i][2]] for i in range(3)]
+        # print "row", row_number, best_matches, [max(edge_sum[best_matches[i][2]-2: best_matches[i][2]+2]) for i in range(3)]
         if len(matched_icons) == 0:
             return
         elif len(matched_icons) == 1:
+            if matched_icons[0][2] < 177:  # todo save this as a constant. The smallest x value of the character 2 icon (when player 2 name is very long)
+                return
             return self._generate_suicide_killfeed(matched_icons)
         else:
             return self._generate_non_suicide_killfeed(matched_icons[:2])
@@ -122,9 +129,9 @@ class FrameAnalyzer:
             _, max_val, _, max_loc = cv2.minMaxLoc(match_result)
             result.append([object_name, max_val, max_loc[0]])
             half_mask_width = 5
-            mask_index_left =  max((max_loc[0] - half_mask_width, 0))
+            mask_index_left = max((max_loc[0] - half_mask_width, 0))
             mask_index_right = min((max_loc[0] + half_mask_width + 1, 1270-963-34))  # TODO link this to killfeed constant of this resolution
-            try:
+            try:  # todo remove try catch block
                 match_result[0, mask_index_left: mask_index_right] = [-1] * (mask_index_right - mask_index_left)
             except:
                 print mask_index_left, mask_index_right
@@ -137,11 +144,10 @@ class FrameAnalyzer:
         return result
 
     def _generate_suicide_killfeed(self, matched_icons):
-        suicider = matched_icons[0]
-        return overwatch.KillFeed("test", self.time, character2=suicider[0], event="suicide")
+        return overwatch.KillFeed("test", self.time, character2=matched_icons[0][0], event="suicide")
 
     def _generate_non_suicide_killfeed(self, best_matches):
-        try:
+        try:  # todo remove try catch block
             if best_matches[0][2] > best_matches[1][2]:
                 best_matches = [best_matches[1], best_matches[0]]
         except:
@@ -150,9 +156,8 @@ class FrameAnalyzer:
         return overwatch.KillFeed("test", self.time, character1=best_matches[0][0], character2=best_matches[1][0])
 
 
-
 if __name__ == '__main__':
-    path = '../../videos/2.mp4'
+    # path = '../../videos/2.mp4'
     path = '../../videos/SDvsNYXL_Preseason.mp4'
     video = video.VideoLoader(path)
     analyze_video(video)
