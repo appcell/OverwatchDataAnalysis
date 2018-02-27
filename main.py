f2bc95dbc79910a55f5c4c792e7037bdcabe763d
Genesis X <genesisx000@gmail.com>
2018-02-27 12:52:18 -0500

    finishing up with multiprocessing

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import time
-from ora.game import Game
 import ora.gui as Gui
+import ora.pool as pool
+import multiprocessing as mp
 
 def main():
     Gui.gui_instance.show()
 
 if __name__ == '__main__':
+    mp.set_start_method('spawn')
+    pool.initPool()
+
     main()
--- ora/pool.py
+++ ora/pool.py
@@ -2,4 +2,6 @@ import multiprocessing as mp
 
 def initPool():
 	global PROCESS_POOL
-	PROCESS_POOL = mp.Pool(processes=mp.cpu_count())
+
+	# More than 12 processes makes no sense with our use case
+	PROCESS_POOL = mp.Pool(processes=min(mp.cpu_count(), 12)) 
