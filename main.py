1505aaa0e0ac5ff628be1b098f4bd93b419106a9
GenesisX <genesisx000@gmail.com>
2018-02-28 14:53:53 -0500

    Use default multiprocessing method for each OS

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import ora.gui as Gui
 import ora.pool as pool
-import multiprocessing as mp
 
 def main():
     Gui.gui_instance.show()
 
 if __name__ == '__main__':
-    mp.set_start_method('spawn')
     pool.initPool()
 
     main()
