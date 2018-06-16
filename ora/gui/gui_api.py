40e45a51ad1d1ccec12e2f6ea67a40f3a8fd6db5
chroming <chroming@live.com>
2018-06-16 11:21:01 +0800

    add gui api

---
+++ ora/gui/gui_api.py
@@ -0,0 +1,40 @@
+
+
+def get_video_info(video_path):
+    """
+    Get video info show in video list.
+    :param video_path:
+    :return: teamname1, teamname2, length
+
+    """
+    pass
+
+
+def publish_analysis():
+    """Run when checked 'Publish your analysis'"""
+    pass
+
+
+def analyze_button_clicked(*args):
+    """
+    Run when click 'ANALYZE' button
+    You should add args as need.
+    """
+    pass
+
+
+def save_button_clicked(*args):
+    """
+    Run when click 'SAVE' button
+    You should add args as need.
+    """
+    pass
+
+
+def auto_check_update_checked():
+    """Run when checked 'Automatically check for update'"""
+    pass
+
+
+
+
