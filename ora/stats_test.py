d678d8eabef062a93b0fb05f71e304509e2f0775
Appcell <eilischu527@gmail.com>
2018-07-23 21:41:34 +0200

    Revert " add some summery"

--- ora/stats_test.py
+++ ora/stats_test.py
@@ -1,5 +1,5 @@
 from stats import single_match_stats
 
-stat_instance = single_match_stats.SingleMatchStats('stats/UITEST_data.zip')
+stat_instance = stats.SingleMatchStats('stats/UITEST_data.zip')
 
-print(stat_instance.teamfight)
+print(stat_instance.teamfight_separations)
