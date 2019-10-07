f0387c5506ebfa8ea39cdc0e876eee8e647c941b
chroming <chroming@live.com>
2019-10-07 18:43:50 +0800

    catch qt error

--- run_qt_gui.py
+++ run_qt_gui.py
@@ -2,6 +2,12 @@ import sys
 from PyQt5 import QtWidgets
 from ora.gui.gui import MainUi
 
+sys._excepthook = sys.excepthook
+def exception_hook(exctype, value, traceback):
+    sys._excepthook(exctype, value, traceback)
+    sys.exit(1)
+sys.excepthook = exception_hook
+
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     with open('./ora/gui/style.qss') as qss:
